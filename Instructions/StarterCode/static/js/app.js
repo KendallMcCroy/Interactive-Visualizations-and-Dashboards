327b500aa10497c287330cd85343b3bfdbfbd713
Kendall <kdmccroy@gmail.com>
2022-01-20 13:56:27 -0500

    moved ploty and d3 to starter code index head

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,5 +1,9 @@
 // Fetch the JSON data and console log it
-d3.json("data/samples.json").then(function(data) {
+d3.json(data/samples.json).then(function(data) {
     console.log(data);
+    
   });
-  
+
+
+
+
