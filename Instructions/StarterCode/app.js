dd25f790f9c8f87b35b2b460404421030cf88553
Kendall <kdmccroy@gmail.com>
2022-01-22 23:28:09 -0500

    Fixed barChartlayout and got it to show

--- Instructions/StarterCode/app.js
+++ Instructions/StarterCode/app.js
@@ -2,47 +2,75 @@ function buildCharts(sample) {
   // Fetch the JSON data and console log it
 d3.json("samples.json").then((data) => {
 
+
   var samples = data.samples;
   var sampledata = samples.filter(sampleObject => sampleObject.id == sample)
   console.log(sampledata[0]);
 
-  
+
+  /////////////////////////////////////////////////////////////////////////////////////////
   // Bubble Chart
+  ////////////////////////////////////////////////////////////////////////////////////////
 
 var results = sampledata[0]
 var otu_id = results.otu_ids
 var otu_labels = results.otu_labels
 var sample_values = results.sample_values
 
-  var bubblechart = [{ 
-    x: otu_id,
-    y: sample_values,
-    mode:"markers",
-    marker: {
+  // var bubblechart = [{ 
+  //   x: otu_id,
+  //   y: sample_values,
+  //   mode:"markers",
+  //   marker: {
 
-            color: otu_id,
-            size: sample_values
+  //           color: otu_id,
+  //           size: sample_values
 
     
-    }
+  //   }
 
-  }]
+  // }]
 
-  var bubblelayout= {
+  // var bubblelayout= {
+  //   // margin:{t:0}
+  //   xaxis: {title: "OTU ID"},
+  //   yaxis: {title: 'Samples Values'},
+  //   title: '<b>Bubble Chart displaying sample values of OTU IDs<b>',
+  //   showlegend: false
+
+  // }
+
+  // Plotly.newPlot("bubble", bubblechart, bubblelayout)
+  
+  /////////////////////////////////////////////////////////////////////
+  //barchart
+  // otu_lables = otu_lables.slice(0, 10).reverse();
+  ////////////////////////////////////////////////////////////////////
+ 
+  var barChart = [{ 
+    x: sample_values,
+    y: otu_id.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
+    // text: otu_lables,
+    type: "bar",
+    orientation: "h"
+  }];
+  
+
+
+  var barChartlayout= {
     // margin:{t:0}
-    xaxis: {title: "OTU ID"},
-    yaxis: {title: 'Samples Values'},
-    title: '<b>Bubble Chart displaying sample values of OTU IDs<b>',
-    showlegend: false
+
+    title: '<b>Top 10 OTUs found in selected Test Subject ID<b>',
+    
 
   }
-
-  Plotly.newPlot("bubble", bubblechart, bubblelayout)
-
-  
+  Plotly.newPlot("bar", barChart, barChartlayout)
 });
 }
 
+///////////////////////////////////////////////////////////////////////////
+//Function init
+///////////////////////////////////////////////////////////////////////////
 
   function init() {
     var selector = d3.select("#selDataset");
